5b18e4b5af5e78ef6631a567e434e439cc0c0d34
Augusto Sandoval <augustocsandoval@gmail.com>
2013-11-16 21:54:16 -0300

    asd

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,7 +1,7 @@
 class OauthController < ApplicationController
   def redirect
-    oauth_redirect_url=root_url
-    session[:access_token] = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
+  	callback="http://rocky-mountain-1051.herokuapp.com/oauth_redirect_url"
+    session[:access_token] = Koala::Facebook::OAuth.new(callback).get_access_token(params[:code]) if params[:code]
 
     redirect_to session[:access_token] ? root_path : failure_path
   end
