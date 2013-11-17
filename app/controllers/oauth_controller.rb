4028f265772b052fe42cad01ff11b5ecf8c2ba60
Augusto Sandoval <augustocsandoval@gmail.com>
2013-11-16 21:16:56 -0300

    intentado conectar a fb

---
+++ app/controllers/oauth_controller.rb
@@ -0,0 +1,9 @@
+class OauthController < ApplicationController
+  def redirect
+    callback="http://rocky-mountain-1051.herokuapp.com/oauth_redirect_url"
+    session[:access_token] = Koala::Facebook::OAuth.new(callback).get_access_token(params[:code]) if params[:code]
+    #redirect_to session[:access_token] ? root_path : failure_path
+    redirect_to root
+
+  end
+end
